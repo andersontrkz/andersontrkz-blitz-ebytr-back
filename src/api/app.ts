f80578fd192674ed1076eccd22fb76052d04660a
andersontrkz <trkz.anderson@gmail.com>
2021-11-04 16:20:55 -0300

    chore(routes): add routes folder

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const routes = require('../routes/index.ts');
+
 const app = express();
 
 app.get('/', (_req, res) => res.send('<h1>Home</h1>'));
 
 app.use(express.json());
+
+app.use(routes);
 
 module.exports = app;
